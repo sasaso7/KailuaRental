3843416c2f90837036d4d25486915b9e2bd5a8ee
Manuvaa <37976125+Manuvaa@users.noreply.github.com>
2020-04-01 14:00:39 +0200

    Add files via upload

---
+++ src/CarProject/Car.java
@@ -0,0 +1,67 @@
+public class Car {
+    private String brand;
+    private String model;
+    private String fuel;
+    private String regNumber;
+    private String regYear;
+    private String odometer;
+
+    public Car(String brand, String model, String fuel, String regNumber, String regYear, String odometer) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = fuel;
+        this.regNumber = regNumber;
+        this.regYear = regYear;
+        this.odometer = odometer;
+    }
+
+    //Getters
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public String getRegYear() {
+        return regYear;
+    }
+
+    public String getOdometer() {
+        return odometer;
+    }
+
+    //Setters
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public void setRegYear(String regYear) {
+        this.regYear = regYear;
+    }
+
+    public void setOdometer(String odometer) {
+        this.odometer = odometer;
+    }
+}
