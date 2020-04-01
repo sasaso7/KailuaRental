ce0979ff0db825a245d02d41ada7044b9f63a028
Manuvaa <37976125+Manuvaa@users.noreply.github.com>
2020-04-01 13:56:01 +0200

    Udkast til projekt Java

---
+++ src/CarMain.java
@@ -0,0 +1,5 @@
+public class CarMain {
+    public static void main(String[] args) {
+        CarMenu.Menu();
+    }
+}
---
+++ src/CarMenu.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarMenu {
+    public static void Menu(){
+        Scanner console = new Scanner(System.in);
+        boolean isRunning = true;
+
+        //Creates Arrays, Reads files, Creates objects, fills Array
+        ArrayList<Luxury> luxuryArray = new ArrayList<>();
+        FileHandler.luxuryReadFromFile(luxuryArray);
+
+        do{
+            System.out.println("1. For at printe liste ud");
+            System.out.println("2. For at tilføje en bil");
+            int menuChoice = console.nextInt();
+
+            switch(menuChoice){
+                case 1:
+                    printList(luxuryArray);
+                    break;
+                case 2:
+                    Luxury.createLuxury(luxuryArray);
+                    break;
+                case 3:
+                    isRunning = false;
+                    break;
+            }
+            break;
+
+
+        }while(isRunning);
+    }
+    public static void printList(ArrayList<?> list){
+        for (Object e: list){
+            System.out.println(e);
+        }
+    }
+}
