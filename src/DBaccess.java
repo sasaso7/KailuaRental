57e492e297d09283ebcf22a26e2daf8b868cfca5
sasaso7 <ydo197@gmail.com>
2020-04-03 11:47:00 +0200

    try try

--- src/DBaccess.java
+++ src/DBaccess.java
@@ -3,8 +3,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 public class DBaccess {
 
@@ -95,4 +98,58 @@ public class DBaccess {
                 System.exit(1);  // terminate program
             }
         }
+    public static void insertRenters(ArrayList<Renters> renters){
+        Scanner in = new Scanner(System.in);
+        try {
+            con = null;
+            Statement s = null;
+            Class.forName(JDBC_DRIVER);
+
+            con = DriverManager.getConnection(DATABASE_URL, "root", "1234");
+            s = con.createStatement();
+            //Her bestemmes renterID
+            int RenterIDCounter = renters.size();
+                    int renterID = RenterIDCounter+1;
+            System.out.println("Please enter your first name");
+                    String renterFirstName = in.next();
+            System.out.println("Please enter your last name");
+                    String renterLastName = in.nextLine();
+            System.out.println("Please enter your phone");
+                    String phone = in.nextLine();
+            System.out.println("Please enter your E-mail");
+                    String mail = in.nextLine();
+            System.out.println("Please enter your address");
+                    String address = in.nextLine();
+            System.out.println("Please enter your city");
+                    String city = in.nextLine();
+            System.out.println("Please enter your zip");
+                    String zip = in.nextLine();
+            System.out.println("Please enter your drivers licence number");
+                    String licenceNumber = in.nextLine();
+            System.out.println("Please enter when your drivers got registered in YYYY-MM-DD");
+                    String licenceSinceFirst = in.nextLine();
+                    Date licenceSince = new SimpleDateFormat("YYYY-MM-DD").parse(licenceSinceFirst);
+                    //nu laves objektet
+                    Renters addman = new Renters(renterID, renterFirstName, renterLastName, phone, mail, address, city, zip, licenceNumber, licenceSince);
+                    //objektet tilføjes på vores arrayliste
+                    renters.add(addman);
+                    s.executeQuery("INSERT INTO renters VALUES  ")
+            s.close();
+            con.close();
+
+        }
+        catch(SQLException sqlex) {
+            try {
+                System.out.println(sqlex.getMessage());
+                con.close();
+                System.exit(1);  // terminate program
+            }
+            catch(SQLException sql){}
+        }
+        catch (ClassNotFoundException noClass) {
+            System.err.println("Driver Class not found");
+            System.out.println(noClass.getMessage());
+            System.exit(1);  // terminate program
+        }
+    }
 }
