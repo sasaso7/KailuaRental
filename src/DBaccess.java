318371e2968e4ea5abc6111b59e55bcccd740d58
Ostefar <56957832+Ostefar@users.noreply.github.com>
2020-04-04 10:43:38 +0200

    preparedStm renters

--- src/DBaccess.java
+++ src/DBaccess.java
@@ -1,11 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -21,7 +18,7 @@ public class DBaccess {
             Statement s = null;
             Class.forName(JDBC_DRIVER);
 
-            con = DriverManager.getConnection(DATABASE_URL, "root", "1234");
+            con = DriverManager.getConnection(DATABASE_URL, "root", "ElfenbenNisse19");
             s = con.createStatement();
 
             ResultSet rs = s.executeQuery("SELECT renter_name,  renter_city  from renters");
@@ -56,7 +53,7 @@ public class DBaccess {
                 Statement s = null;
                 Class.forName(JDBC_DRIVER);
 
-                con = DriverManager.getConnection(DATABASE_URL, "root", "1234");
+                con = DriverManager.getConnection(DATABASE_URL, "root", "ElfenbenNisse19");
                 s = con.createStatement();
 
                 ResultSet rs = s.executeQuery("SELECT renter_id, renter_first_name, renter_last_name, renter_phone, renter_mail, renter_adress, renter_city, renter_zip, renter_licence_number, renter_licence_since  FROM renters");
@@ -102,39 +99,42 @@ public class DBaccess {
         Scanner in = new Scanner(System.in);
         try {
             con = null;
-            Statement s = null;
             Class.forName(JDBC_DRIVER);
+            con = DriverManager.getConnection(DATABASE_URL, "root", "ElfenbenNisse19");
 
-            con = DriverManager.getConnection(DATABASE_URL, "root", "1234");
-            s = con.createStatement();
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+
+            String query = " insert into users (renter_first_name, renter_last_name, renter_phone, renter_mail, renter_adress, renter_city, renter_zip, renter_licence_number, renter_license_since)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+
             //Her bestemmes renterID
             int RenterIDCounter = renters.size();
                     int renterID = RenterIDCounter+1;
             System.out.println("Please enter your first name");
-                    String renterFirstName = in.next();
+                    preparedStmt.setString (1, "in.nextLine");
             System.out.println("Please enter your last name");
-                    String renterLastName = in.nextLine();
+                    preparedStmt.setString (2, "in.nextLine");
             System.out.println("Please enter your phone");
-                    String phone = in.nextLine();
+                    preparedStmt.setString (3, "in.nextLine");
             System.out.println("Please enter your E-mail");
-                    String mail = in.nextLine();
+                    preparedStmt.setString (4, "in.nextLine");
             System.out.println("Please enter your address");
-                    String address = in.nextLine();
+                    preparedStmt.setString (5, "in.nextLine");
             System.out.println("Please enter your city");
-                    String city = in.nextLine();
+                    preparedStmt.setString (6, "in.nextLine");
             System.out.println("Please enter your zip");
-                    String zip = in.nextLine();
-            System.out.println("Please enter your drivers licence number");
-                    String licenceNumber = in.nextLine();
-            System.out.println("Please enter when your drivers got registered in YYYY-MM-DD");
-                    String licenceSinceFirst = in.nextLine();
-                    Date licenceSince = new SimpleDateFormat("YYYY-MM-DD").parse(licenceSinceFirst);
-                    //nu laves objektet
-                    Renters addman = new Renters(renterID, renterFirstName, renterLastName, phone, mail, address, city, zip, licenceNumber, licenceSince);
-                    //objektet tilføjes på vores arrayliste
-                    renters.add(addman);
-                    s.executeQuery("INSERT INTO renters VALUES  ")
-            s.close();
+                    preparedStmt.setString (7, "in.nextLine");
+            System.out.println("Please enter your driver licence number");
+                    preparedStmt.setString (8, "in.nextLine");
+            System.out.println("Please enter when your driver licence got registered in YYYY-MM-DD");
+                    preparedStmt.setString (9, "in.nextLine");
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
             con.close();
 
         }
