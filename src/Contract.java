6ac3754c5eefde4ec41ac9277d9575f23a314cc7
sasaso7 <ydo197@gmail.com>
2020-04-09 10:26:50 +0200

    Contractman

---
+++ src/Contract.java
@@ -0,0 +1,9 @@
+public class Contract {
+    Car contractCar;
+    Renters contractRenters;
+
+    public Contract(Car contractCar, Renters contractRenters){
+        this.contractCar = contractCar;
+        this.contractRenters = contractRenters;
+    }
+}
